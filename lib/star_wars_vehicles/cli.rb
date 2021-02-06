c17f93fe8156d6eed4062fa2708692838f7c7406
Javier <coyote176@msn.com>
2021-02-05 23:03:21 -0700

    Cli file

--- lib/star_wars_vehicles/cli.rb
+++ lib/star_wars_vehicles/cli.rb
@@ -1,9 +1,50 @@
 class StarWarsVehicles::CLI
    
     def call
-        puts "Welcome to the Force"
-        puts "Pick a vehicle form the list"
+        puts "Welcome Starfigthers"
+        puts "Enter list to pick a vehicle."
         puts "To exit, enter 'exit'"
+        menu
+    end
+
+    def menu
+        input = gets.strip
+
+        if input == "list"
+            vehicle_list
+            menu
+        elsif
+            input == "exit"
+            sign_off
+        else
+            wrong_data
+            menu
+        end
+
+    end
+
+    def vehicle_list
+        puts "vehicle 1"
+        puts "vehicle 2"
+        puts "vehicle 3"
+        puts ""
+        puts ""
+        puts "Which starfighter would you want more information?"
+        input = gets.strip
+        vehicle_selection(input)
+    end
+
+    def vehicle_selection(starfighter)
+        puts "#{starfighter}"
+        
+    end
+
+    def sign_off
+        puts "Thank You, May the Force Be With You!"
+    end
+
+    def wrong_data
+        puts "OPPS wrong entery"
     end
 
 end
